3e91840f7a2fa268d5e40e0f02ab49a0deeaddd6
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-07-14 12:08:20 +0200

    added category model and zod schema validation

---
+++ src/models/Category.ts
@@ -0,0 +1,21 @@
+import {Schema, model, set} from 'mongoose';
+
+const CategorySchema = new Schema ({
+    name: {
+        type: String,
+        required: [true, 'Categoryname is required'],
+        trim: true,
+        minLength: [2, 'Min length is 2 chars'],
+        maxLength: [100, 'Max length 100 chars']
+    }
+});
+
+// every time a User document is serialized to JSON (e.g. res.json(user)), the response has a clean id string instead of both _id and id.
+set('toJSON', {
+  virtuals: true,
+  transform: (doc, converted) => {
+    delete (converted as Partial<typeof converted>)._id;
+  }
+});
+
+export default model('Category', CategorySchema);
