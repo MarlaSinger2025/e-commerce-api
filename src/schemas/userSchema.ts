96fe77c9662857dc2d186950e62fb5351bac8163
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-07-15 11:11:17 +0200

    Added schema for updating only partial fields of user to userSchemas and userRoutes

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -6,4 +6,9 @@ export const userInputSchema = z.strictObject({
     password: z.string().min(8, 'Must be at least 8 chars')
 });
 
+// Schema for User update - partial() makes every input field optial
+export const updatedUserSchema = userInputSchema.partial();
+
+
+export type UpdatedUserInput = z.infer<typeof updatedUserSchema>;
 export type UserInput = z.infer<typeof userInputSchema>;
