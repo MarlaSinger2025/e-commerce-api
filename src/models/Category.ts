a74dc412135fda524c05a2f1d296a229305d9af4
MarlaSinger2025 <Marla_Singer_2020@web.de>
2026-07-15 14:13:51 +0200

    Added collation index to Category Modul Schema for checking name uniquness and making sure capitalized and not capitalized words are handled the same, added zod transorm to categorySchema so every input gets formatted into upper first leter and the rest lower letters

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -5,11 +5,19 @@ const CategorySchema = new Schema ({
         type: String,
         required: [true, 'Categoryname is required'],
         trim: true,
-        unique: true,
         minLength: [2, 'Min length is 2 chars'],
         maxLength: [100, 'Max length 100 chars']
     }
 });
+
+//Adding case-insensitive unique index using 'collation' so e.g. "Clothing" and "clothing" are considered 
+// the same value and can throw duplicate error
+CategorySchema.index(
+  { name: 1},
+  { unique: true,
+    collation: { locale: 'en', strength: 2},
+  }
+);
 
 // every time a User document is serialized to JSON (e.g. res.json(user)), the response has a clean id string instead of both _id and id.
 set('toJSON', {
